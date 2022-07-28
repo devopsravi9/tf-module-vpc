cf0a5567aa56dd36463d2d06f757135c96d1c334
Ravi <ravi@gmail.com>
2022-07-28 16:07:50 +0530

    ec2 code

--- route.tf
+++ route.tf
@@ -25,3 +25,9 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private.*.id[count.index]
   route_table_id = aws_route_table.private.id
 }
+
+resource "aws_route" "igw-route-to-public-subnet" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
