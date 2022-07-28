cd571803b26d705c6126f6d3e73a7689bdfa9823
Ravi <ravi@gmail.com>
2022-07-28 16:53:37 +0530

    ec2 code

--- route.tf
+++ route.tf
@@ -32,8 +32,26 @@ resource "aws_route" "igw-route-to-public-subnet" {
   gateway_id = aws_internet_gateway.igw.id
 }
 
-resource "aws_route" "igw-route-to-private-subnet" {
+resource "aws_route" "ngw-route-to-private-subnet" {
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.ngw.id
+}
+
+resource "aws_route" "peer-route-to-private-subnet" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = var.DEFAULT_VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+resource "aws_route" "peer-route-to-public-subnet" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = var.DEFAULT_VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+resource "aws_route" "ngw-route-to-default-vpc-subnet" {
+  route_table_id            = var.DEFAULT_VPC_RT
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
