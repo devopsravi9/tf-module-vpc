cef2e35e4542223bf8151628abf9138dbe9454e0
Ravi <ravi@gmail.com>
2022-07-28 15:34:19 +0530

    ec2 code

---
+++ route.tf
@@ -0,0 +1,25 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.PROJECT}-${var.ENV}-public-rt"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.PROJECT}-${var.ENV}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public.*.id[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.private.*.id[count.index]
+  route_table_id = aws_route_table.private.id
+}
